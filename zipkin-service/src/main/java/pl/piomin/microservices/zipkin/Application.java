79280c588a15ebe2b7e1dc1a12a1ffcda1092100
Luke Lanterme <llanterme@pnp.co.za>
2017-12-01 10:17:23 +0700

    first commit

---
+++ zipkin-service/src/main/java/pl/piomin/microservices/zipkin/Application.java
@@ -0,0 +1,18 @@
+package pl.piomin.microservices.zipkin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import zipkin.server.EnableZipkinServer;
+
+@SpringBootApplication
+@EnableZipkinServer
+
+//ds
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
